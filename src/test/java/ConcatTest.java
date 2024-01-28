b53fe50681724edb69a3f651a61cca2b52769823
eugeneburdukov <burdukov.eugene@gmail.com>
2024-01-28 21:40:41 +0200

    12th commit

--- src/test/java/ConcatTest.java
+++ src/test/java/ConcatTest.java
@@ -1,3 +1,4 @@
+import io.qameta.allure.Feature;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
@@ -5,13 +6,21 @@ import org.testng.annotations.Test;
 
 public class ConcatTest {
     @Test
+    @Feature("First Test")
     public void test1() {
         Concat concat = new Concat();
         Assert.assertEquals("Ridiculous", Concat.concatinate("Ridi", "culous"));
     }
 
     @Test
+    @Feature("Second Test")
     public void test2() {
         Assert.assertEquals("Sectumsempra", Concat.concatinate("Sectum", "sempra"));
+    }
+
+    @Test
+    @Feature("Third Test")
+    public void test3() {
+        Assert.assertEquals("nono", Concat.concatinate("yes", "yes"));
     }
 }
