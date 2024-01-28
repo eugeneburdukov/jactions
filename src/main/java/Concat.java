79c343d3ac7e11d56a54f5cda90843342f9ff967
eugeneburdukov <burdukov.eugene@gmail.com>
2024-01-28 20:31:11 +0200

    2nd

--- src/main/java/Concat.java
+++ src/main/java/Concat.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class Concat {
+public class Concat {
+    public static String concatinate(String line1, String line2){
+        return line1 + line2;
+    }
 }
--- src/test/java/ConcatTest.java
+++ src/test/java/ConcatTest.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class ConcatTest {
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ConcatTest {
+    @Test
+    public void test1() {
+        Concat concat = new Concat();
+        Assert.assertEquals("Ridiculous", Concat.concatinate("Ridi", "culouss"));
+    }
 }
